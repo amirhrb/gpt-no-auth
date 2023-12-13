d25dcba204c0966fc474d2d5cf68016fecc8b3e0
amirhrb <amirrabi70@gmail.com>
2023-12-13 18:29:03 +0330

    handle sanction error

--- pages/api/generate-text.js
+++ pages/api/generate-text.js
@@ -1,11 +1,10 @@
-import { Configuration, OpenAIApi } from 'openai';
+import { Configuration, OpenAIApi } from "openai";
 
 export default async function handler(req, res) {
-  console.log('offline!');
-  if (req.method !== 'POST')
+  if (req.method !== "POST")
     return res
       .status(403)
-      .json({ status: 'failure', message: 'درخواست اشتباه بود' });
+      .json({ status: "failure", message: "درخواست اشتباه بود" });
 
   const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
@@ -13,46 +12,60 @@ export default async function handler(req, res) {
   const openai = new OpenAIApi(configuration);
 
   const messages =
-    req.body.messages.map(message => {
+    req.body.messages.map((message) => {
       return {
         role: message.role,
         content: message.content,
       };
-    }) || '';
+    }) || "";
   if (messages.length === 0) {
     return res.status(400).json({
-      status: 'failure',
+      status: "failure",
       error: {
-        message: 'یک سوال بپرسید',
+        message: "یک سوال بپرسید",
       },
     });
   }
   try {
     const completion = await openai.createChatCompletion({
-      model: 'gpt-3.5-turbo',
+      model: "gpt-3.5-turbo",
       messages: [
         {
-          role: 'system',
+          role: "system",
           content:
-            'please always put all responses and codes in proper markdown format that matches the code language',
+            "please always put all responses and codes in proper markdown format that matches the code language",
         },
         ...messages,
       ],
     });
     res.status(200).json({
-      status: 'success',
+      status: "success",
       result: completion.data.choices[0].message,
       question: messages[messages.length - 1],
     });
   } catch (error) {
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
-      res.status(error.response.status).json(error.response.data);
+      if (
+        error.response.status === 403 &&
+        error.response.statusText === "Forbidden"
+      ) {
+        console.log("forbidden");
+
+        res
+          .status(error.response.status)
+          .json({
+            status: "failure",
+            error: { message: "vpn لازمه متاسفانه!" },
+          });
+      } else {
+        res.status(error.response.status).json(error.response.data);
+      }
     } else {
       res.status(500).json({
-        status: 'failure',
+        status: "failure",
         error: {
-          message: 'خطایی در درخواست رخ داده',
+          message: "خطایی در درخواست رخ داده",
         },
       });
     }
